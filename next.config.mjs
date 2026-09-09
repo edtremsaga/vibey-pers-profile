ac018bb9b93eda36ad1649702b8d7a2cc0694b4b
Edward Tremblay <edwardktremblay@gmail.com>
2026-09-09 13:56:44 -0700

    Route drvr8.band homepage to existing band page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/',
+          has: [{ type: 'host', value: 'drvr8\\.band' }],
+          destination: '/drvr8',
+        },
+      ],
+    };
+  },
+};
 
 export default nextConfig;
